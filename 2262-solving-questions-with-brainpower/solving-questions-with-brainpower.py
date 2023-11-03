b34b8a6794ac92ee03583822dc1eabb279bc28d6
Salma <salmasaa02@gmail.com>
2023-11-04 03:17:52 +0530

    Time: 1377 ms (81.47%) | Memory: 62.7 MB (54.08%) - LeetSync

---
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
@@ -0,0 +1,7 @@
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        res=[0] * (len(questions) + 1)
+        for i in range(len(questions)-1, -1, -1):
+            point,solve = questions[i][0], questions[i][1]
+            res[i] = max(point + res[min(solve+i+1,len(questions))],res[i+1])
+        return res[0]
