28311b889915fe8ea7cc081c35274fcf0c53c553
Salma <salmasaa02@gmail.com>
2023-11-04 14:59:31 +0530

    Time: 1602 ms (7.19%) | Memory: 366.7 MB (5.23%) - LeetSync

--- 1290-make-array-strictly-increasing/make-array-strictly-increasing.py
+++ 1290-make-array-strictly-increasing/make-array-strictly-increasing.py
@@ -1,29 +1,18 @@
-from functools import lru_cache
-from typing import List
-
 class Solution:
     def makeArrayIncreasing(self, arr1: List[int], arr2: List[int]) -> int:
-        @lru_cache(None)
-        def dp(a, b, c):
-            ans = float('inf')  
-            if c:
-                prev = arr2[b - 1]
-            else:
-                prev = arr1[a - 1]
-            if a == len(arr1):
-                return 0
-            if prev < arr1[a]:
-                ans = min(ans, dp(a + 1, b, 0))
-            if b < len(arr2) and prev < arr2[b]:
-                ans = min(ans, 1 + dp(a + 1, b + 1, 1))
-            if b < len(arr2) and prev >= arr2[b]:
-                ans = min(ans, dp(a, b + 1, c)) 
+        @cache
+        def dp(a,b,c):
+            ans = math.inf
+            if c: prev = arr2[b-1]
+            else: prev = arr1[a-1]
+            if a == len(arr1): return 0
+            if prev < arr1[a]: ans = min(ans, dp(a+1,b,0))
+            if b < len(arr2) and prev < arr2[b]: ans = min(ans, 1+dp(a+1,b+1,1))
+            if b < len(arr2) and prev >= arr2[b]:ans = min(ans, dp(a,b+1,c))
             return ans
 
         arr2.sort()
-        ans = dp(1, 0, 0)
-        if arr2[0] < arr1[0]:
-            ans = min(ans, 1 + dp(1, 1, 1))
-        if ans == float('inf'): 
-            return -1
+        ans = dp(1,0,0)
+        if arr2[0] < arr1[0]: ans = min(ans, 1+dp(1,1,1))
+        if ans == math.inf: return -1
         return ans
