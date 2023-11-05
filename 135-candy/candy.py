e0d41e07576729e285975f4869bc57a95f3f4f40
Salma <salmasaa02@gmail.com>
2023-11-05 19:42:56 +0530

    Time: 148 ms (37.79%) | Memory: 19.1 MB (79.05%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -2,30 +2,20 @@ from typing import List
 
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        """
-        Calculate the minimum number of candies required to distribute to children.
-
-        Args:
-        ratings (List[int]): An array of ratings for each child.
-
-        Returns:
-        int: The minimum number of candies needed.
-        """
         n = len(ratings)
         candies = [1] * n
 
-        # Left to right pass
+        # First pass: Left to right
         for i in range(1, n):
             if ratings[i] > ratings[i - 1]:
                 candies[i] = candies[i - 1] + 1
 
-        total_candies = candies[n - 1]
-
-        # Right to left pass
+        # Second pass: Right to left
         for i in range(n - 2, -1, -1):
             if ratings[i] > ratings[i + 1]:
                 candies[i] = max(candies[i], candies[i + 1] + 1)
 
-            total_candies += candies[i]
-
+        # Calculate the total number of candies
+        total_candies = sum(candies)
         return total_candies
+
