de9acea8fc162f93c695b0978a986fd4daf13699
Salma <salmasaa02@gmail.com>
2023-11-08 22:06:40 +0530

    Time: 35 ms (74.85%) | Memory: 16.3 MB (62.20%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        wordSet = set(wordDict)
+        n = len(s)
+
+        @lru_cache(maxsize=None)
+        def wordBreakDP(start):
+            if start == n:
+                return [""]
+            sentences = []
+            for end in range(start + 1, n + 1):
+                word = s[start:end]
+                if word in wordSet:
+                    for sentence in wordBreakDP(end):
+                        if sentence:
+                            sentences.append(word + " " + sentence)
+                        else:
+                            sentences.append(word)
+            return sentences
+
+        return wordBreakDP(0)        
