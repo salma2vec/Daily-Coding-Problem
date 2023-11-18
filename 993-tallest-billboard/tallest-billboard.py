d11306adb120da9bf24e5a930d319e013c568aab
Salma <salmasaa02@gmail.com>
2023-11-18 23:07:14 +0530

    Time: 438 ms (46.50%) | Memory: 17 MB (51.26%) - LeetSync

--- 993-tallest-billboard/tallest-billboard.py
+++ 993-tallest-billboard/tallest-billboard.py
@@ -1,9 +1,26 @@
 class Solution:
     def tallestBillboard(self, rods: List[int]) -> int:
-        @lru_cache(None)
-        def dp(i, d):
-            if i == len(rods): return 0 if d == 0 else -inf
-            return max(dp(i + 1, d), rods[i] + dp(i + 1, d + rods[i]), dp(i + 1, d - rods[i]))
-        return dp(0, 0)
+        dp = {0: 0}
 
-        
+        for rod in rods:
+            copy_dp = dp.copy()
+
+            for difference, taller in dp.items():
+                if (difference + rod) in copy_dp:
+                    copy_dp[difference + rod] = max(copy_dp[difference + rod], taller + rod)
+                else:
+                    copy_dp[difference + rod] = max(0, taller + rod)
+
+                shorter = taller - difference
+                new_difference = abs(shorter + rod - taller)
+                new_taller = max(shorter + rod, taller)
+
+                if new_difference in copy_dp:
+                    copy_dp[new_difference] = max(copy_dp[new_difference], new_taller)
+                else:
+                    copy_dp[new_difference] = max(0, new_taller)
+
+            dp = copy_dp
+
+        result = dp[0]
+        return result
