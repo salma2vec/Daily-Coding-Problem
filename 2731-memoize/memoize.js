dad78f75a68793aac80168be2a541e2ea90a5559
Salma <salmasaa02@gmail.com>
2023-11-18 15:28:58 +0530

    Time: 273 ms (84.23%) | Memory: 108.1 MB (42.29%) - LeetSync

---
+++ 2731-memoize/memoize.js
@@ -0,0 +1,30 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const cache = new Map();
+
+    return function (...args) {
+        const key = JSON.stringify(args);
+
+        if (cache.has(key)) {
+            return cache.get(key);
+        } else {
+            const result = fn.apply(null, args);
+            cache.set(key, result);
+            return result;
+        }
+    };
+}
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
