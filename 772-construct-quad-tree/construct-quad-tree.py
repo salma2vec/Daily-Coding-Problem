ef021a35a3a4a4a40149e17ace56a79ae1def11e
Salma <salmasaa02@gmail.com>
2023-11-19 21:43:58 +0530

    Time: 107 ms (56.96%) | Memory: 17.3 MB (31.70%) - LeetSync

---
+++ 772-construct-quad-tree/construct-quad-tree.py
@@ -0,0 +1,18 @@
+class Solution:
+    def construct(self, grid: List[List[int]]) -> 'Node':
+        def dfs(n, r, c):
+            allSame = True
+            for i in range(n):
+                for j in range(n):
+                    if grid[r][c] != grid[r + i][c + j]:
+                        allSame = False
+                        break
+            if allSame:
+                return Node(grid[r][c], True)
+            n = n // 2
+            topleft = dfs(n, r, c)
+            topright = dfs(n, r, c + n)
+            bottomleft = dfs(n, r + n, c)
+            bottomright = dfs(n, r + n, c + n)
+            return Node(0, False, topleft, topright, bottomleft, bottomright)
+        return dfs(len(grid), 0, 0) 
