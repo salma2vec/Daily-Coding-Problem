01c906aaea3852728febcba7c9597fd6b10f2def
Salma <salmasaa02@gmail.com>
2023-11-30 01:07:34 +0530

    Time: 116 ms (83.30%) | Memory: 77.7 MB (14.17%) - LeetSync

---
+++ 2742-group-by/group-by.ts
@@ -0,0 +1,19 @@
+declare global {
+    interface Array<T> {
+        groupBy(fn: (item: T) => string): Record<string, T[]>
+    }
+}
+
+Array.prototype.groupBy = function(fn) {
+    const hash = {}
+    for (let item of this){
+        const key = fn(item)
+        hash[key] ||= []
+        hash[key].push(item)
+    }
+    return hash
+}
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
