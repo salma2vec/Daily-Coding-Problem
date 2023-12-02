5af73675702ce0410d5f6719b703aa49511a732e
Salma <salmasaa02@gmail.com>
2023-12-02 16:28:59 +0530

    Time: 44 ms (55.62%) | Memory: 16.8 MB (21.20%) - LeetSync

---
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
@@ -0,0 +1,18 @@
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> list[int]:
+
+        def find_rpath(src: TreeNode | None, dst: TreeNode | None) -> Iterator[TreeNode]:
+            path = find_rpath(src.left, dst) or find_rpath(src.right, dst) if src else []
+            return path.append(src) or path if path or (src == dst != None) else path
+        
+        def level_order(root: TreeNode, parents: Mapping[TreeNode, TreeNode] = None) -> Iterator[tuple[TreeNode]]:
+            level, seen, parents = (root,), {root, None}, parents or {}
+            while level:
+                yield level
+                level = tuple(child for node in level for child in (node.left, node.right, parents.get(node)) if child not in seen)
+                seen.update(level)
+
+        path = find_rpath(root, target)
+        parents_map = dict(pairwise(path))
+        kth_level = next(islice(level_order(target, parents_map), k, None), [])
+        return [node.val for node in kth_level]
