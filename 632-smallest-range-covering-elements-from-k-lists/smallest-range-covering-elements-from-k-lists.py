f8664a751b0cfc7b6ef5acb31693738d4026b7b0
Salma <salmasaa02@gmail.com>
2023-12-05 21:40:46 +0530

    Time: 199 ms (95.19%) | Memory: 24.9 MB (6.41%) - LeetSync

---
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+class Solution:
+    def smallestRange(self, nums: List[List[int]]) -> List[int]:
+        temp = []
+        for i, arr in enumerate(nums):
+            for n in arr:
+                temp.append((n, i))
+
+        temp.sort()
+
+        k = len(nums)
+        ans = (temp[0][0], temp[-1][0])
+        best = temp[-1][0] - temp[0][0]
+
+        seen = defaultdict(int)
+        start = 0
+        for i, (x, idx) in enumerate(temp):
+            seen[idx] += 1
+            if i == 0:
+                continue
+
+            if len(seen) < k:
+                continue
+            else:
+                while len(seen) == k:
+                    if x - temp[start][0] < best:
+                        best = x - temp[start][0]
+                        ans = (temp[start][0], x)
+                    
+                    y = temp[start][1]
+                    seen[y] -= 1
+                    if seen[y] == 0:
+                        del seen[y]
+                    start += 1
+
+        return ans
