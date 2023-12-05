68455b9076d85aa2ee350d2fbb651adeec721165
Salma <salmasaa02@gmail.com>
2023-12-05 21:55:37 +0530

    Time: 240 ms (60.74%) | Memory: 17.1 MB (91.11%) - LeetSync

--- 855-count-unique-characters-of-all-substrings-of-a-given-string/count-unique-characters-of-all-substrings-of-a-given-string.py
+++ 855-count-unique-characters-of-all-substrings-of-a-given-string/count-unique-characters-of-all-substrings-of-a-given-string.py
@@ -1,17 +1,12 @@
 class Solution:
     def uniqueLetterString(self, s: str) -> int:
-        li = []
-        for i in range(26):
-            temp = []
-            temp.append(-1) 
-            li.append(temp)
-        for i in range(len(s)):
-            li[ord(s[i]) - 65].append(i)
-        ans = 0
-        for each in li:
-            each.append(len(s))
-            for i in range(1, len(each) - 1): 
-                ans += ((each[i] - each[i - 1]) * (each[i + 1] - each[i]))
-        return ans        
-
+        res = 0
+        res_temp = 0
+        dic = defaultdict(int)
+        dic_dc = defaultdict(int)
+        for i, ch in enumerate(s):
+            res_temp += i + 1 - dic[ch]*2 + dic_dc[ch]
+            dic_dc[ch], dic[ch] = dic[ch], i+1
+            res += res_temp
+        return res
 
