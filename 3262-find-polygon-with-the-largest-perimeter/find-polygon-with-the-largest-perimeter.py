f5f295f5284794623fc3ae2df2273fc11978acaa
Salma <salmasaa02@gmail.com>
2023-12-24 22:32:15 +0530

    Time: 593 ms (100.00%) | Memory: 32.6 MB (100.00%) - LeetSync

---
+++ 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.py
@@ -0,0 +1,19 @@
+from collections import defaultdict as dd
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        N=len(nums)
+        nums = list(sorted(nums))
+        pref_sum = dd(lambda : None )
+        p_i = None
+        maxPossiblePolygonPermieter = -1
+        for i in range(N):
+            num = nums[i]
+            if(pref_sum[p_i] is not None):
+                if( pref_sum[p_i] > num):
+                    if(i>=2):
+                        maxPossiblePolygonPermieter=pref_sum[p_i] + num
+            pref_sum[i] = num  +  (0 if(pref_sum[p_i] is None) else pref_sum[p_i])
+
+            del pref_sum[p_i]
+            p_i = i
+        return maxPossiblePolygonPermieter
