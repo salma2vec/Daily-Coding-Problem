bc41105740f94e2ca6f8d84480ad57b3a17e1251
Salma Shaik <salmasaa02@gmail.com>
2024-05-27 02:48:50 +0530

    Time: 3 ms (67.47%) | Memory: 2.7 MB (90.51%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.go
@@ -0,0 +1,23 @@
+func searchMatrix(matrix [][]int, target int) bool {
+    if len(matrix) == 0 || len(matrix[0]) == 0 {
+        return false
+    }
+
+    rows, cols := len(matrix), len(matrix[0])
+    left, right := 0, rows*cols-1
+
+    for left <= right {
+        mid := (left + right) / 2
+        midValue := matrix[mid/cols][mid%cols]
+
+        if midValue == target {
+            return true
+        } else if midValue < target {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+
+    return false
+}
